445e25ddaa4a5b5301c4574a8354d6cc03045462
Dmitry Pluzhnikov <pdymmm@gmail.com>
2026-01-07 17:31:00 +0100

    first commit

---
+++ src/main/java/callapp1/CallApp_1.java
@@ -0,0 +1,56 @@
+package callapp1;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class CallApp_1 {
+
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(
+          System.getenv().getOrDefault("PORT", "8080")
+        );
+
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server avviato sulla porta " + port);
+
+        while (true) {
+            Socket client = serverSocket.accept();
+            handleClient(client);
+        }
+    }
+
+    private static void handleClient(Socket client) {
+        try (
+          BufferedReader in = new BufferedReader(
+            new InputStreamReader(client.getInputStream()));
+          BufferedWriter out = new BufferedWriter(
+            new OutputStreamWriter(client.getOutputStream()))
+        )
+        {
+            String line;
+            while (!(line = in.readLine()).isEmpty()) {
+                System.out.println(line); // stampa request
+            }
+
+            String body = "{\"message\":\"Ciao dal server Java 21 🚀\"}";
+
+            out.write("HTTP/1.1 200 OK\r\n");
+            out.write("Content-Type: application/json\r\n");
+            out.write("Content-Length: " + body.length() + "\r\n");
+            out.write("\r\n");
+            out.write(body);
+            out.flush();
+
+            client.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
